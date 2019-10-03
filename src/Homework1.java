6a4ff38534e0524d62a3911247bbba01163325d6
dcjacks2 <bockielou@hotmail.com>
2019-10-02 22:35:22 -0500

    setting up switch/case user menu interface and file connection

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,15 +1,38 @@
+import java.io.*;
 import java.util.Scanner;
 
 public class Homework1 {
-    public static void main (String [] args){
-        float balance;
-        balance = 1000;
+    public static void main (String [] args) throws IOException {
+        File text = new File("Banklog.txt");
+        Scanner scan = new Scanner(System.in);
+        int option;
+        float balance = 1000f;
+        boolean quit = false;
+        FileWriter fwriter = new FileWriter("Banklog.txt", true);
+        PrintWriter outfile= new PrintWriter((fwriter));
 
-        System.out.println("1. Display balance summary:  ");
-        System.out.println("2. Withdraw money: ");
-        System.out.println("3. Deposit money: ");
-        System.out.println("Enter 0 to exit: ");
 
 
+        while(!quit) {
+            int choice = 0;
+
+            System.out.println("1. Display balance summary:  ");
+            System.out.println("2. Withdraw money: ");
+            System.out.println("3. Deposit money: ");
+            System.out.println("Enter 0 to exit: ");
+
+            choice = scan.nextInt();
+
+            switch(choice){
+                case 1:
+
+                case 2:
+
+                case 3:
+
+                case 0:
+                quit = true;
+            }
+        }
     }
 }
