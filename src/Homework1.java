2879aaa1ade6b6748419a6b2c5d3e77ed2594de8
dcjacks2 <bockielou@hotmail.com>
2019-10-02 21:41:08 -0500

    creating balance and basic menu option

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Homework1 {
     public static void main (String [] args){
-        //update test
-        //second test
+        int balance;
+        balance = 1000;
+
+        System.out.println("1. Display balance summary:  ");
+        System.out.println("2. Withdraw money: ");
+        System.out.println("3. Deposit money: ");
+        System.out.println("Enter 0 to exit: ");
+
+
     }
 }
