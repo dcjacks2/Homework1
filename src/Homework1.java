b64901ab62bb3c8bd233661db639c68d3c46fddb
dcjacks2 <bockielou@hotmail.com>
2019-10-02 21:45:34 -0500

    correcting int to float balance

--- src/Homework1.java
+++ src/Homework1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Homework1 {
     public static void main (String [] args){
-        int balance;
+        float balance;
         balance = 1000;
 
         System.out.println("1. Display balance summary:  ");
